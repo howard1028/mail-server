01beb91036a74575d20791a4f714a7d473597d64
howard1028 <993004@gmail.com>
2022-12-28 16:53:50 +0800

    attachment ok

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import smtplib
 from email import encoders
 from email.mime.text import MIMEText
-from email.mime.base import MIMEBase
-from email.mime.multipart import MIMEMultipart #暫存附件
+from email.mime.application import MIMEApplication#傳送附件
+from email.mime.multipart import MIMEMultipart #傳送mail的各部分
 
 server = smtplib.SMTP('smtp.gmail.com', 587) #host,port
 server.starttls()
@@ -10,7 +10,7 @@ server.ehlo() #start the server
 
 with open('password.txt','r') as f: #讀密碼
     password = f.read()
-server.login('a@g-mail.nsysu.edu.tw',password) 
+server.login('your email@g-mail.nsysu.edu.tw',password) 
 
 #填新郵件
 msg = MIMEMultipart()
@@ -22,14 +22,12 @@ with open('message.txt','r') as f:
 msg.attach(MIMEText(message,'plain')) #內文
 
 #加附件
-filename = 'chenchen.jpg'
-attachment = open(filename,'rb') #open with binary
-p = MIMEBase('application' , 'octet-stream') 
-p.set_payload(attachment.read()) #讀的attachment的payload
-
-encoders.encode_base64(p)
-p.add_header('Content-Disposition' , f'attachment={attachment}') #要傳的payload加上header才能當附件
+file = 'chenchen.jpg'
+p = MIMEApplication(open(file,'rb').read()) 
+p.add_header('Content-Disposition','attachment',filename=file)
 msg.attach(p)
 
+
 text = msg.as_string() 
-server.sendmail('a@g-mail.nsysu.edu.tw','wyi67441@nezid.com',text) #真正寄，回送寄件備份
+server.sendmail('your email@g-mail.nsysu.edu.tw','wyi67441@nezid.com',text) #真正寄，回送寄件備份
+print("Sent successful!")
