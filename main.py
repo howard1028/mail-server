a3bb3ae2b3815d6fd8b822b00b3d4e295df800fa
howard1028 <993004@gmail.com>
2022-12-28 16:38:44 +0800

    can send , but attachment error

--- main.py
+++ main.py
@@ -4,25 +4,24 @@ from email.mime.text import MIMEText
 from email.mime.base import MIMEBase
 from email.mime.multipart import MIMEMultipart #暫存附件
 
-server = smtplib.SMTP('smtp.office365.com', 587) #host,port
-# server.connect("smtp.world4you.com",465)
+server = smtplib.SMTP('smtp.gmail.com', 587) #host,port
+server.starttls()
 server.ehlo() #start the server
 
 with open('password.txt','r') as f: #讀密碼
     password = f.read()
+server.login('a@g-mail.nsysu.edu.tw',password) 
 
-# server.login('m103040072@g-mail.nsysu.edu.tw',password) #有需要密碼才要加
-
+#填新郵件
 msg = MIMEMultipart()
 msg['from'] = 'howard1028'
-msg['to'] = 'dyk31584@cdfaq.com'
-msg['subject'] = 'a Test'
-
-with open('message.txt','r') as f:
+msg['to'] = 'wyi67441@nezid.com' #收件者
+msg['subject'] = 'a Test' #主旨
+with open('message.txt','r') as f: 
     message = f.read()
+msg.attach(MIMEText(message,'plain')) #內文
 
-msg.attach(MIMEText(message,'plain')) #header和text加入message object
-
+#加附件
 filename = 'chenchen.jpg'
 attachment = open(filename,'rb') #open with binary
 p = MIMEBase('application' , 'octet-stream') 
@@ -33,4 +32,4 @@ p.add_header('Content-Disposition' , f'attachment={attachment}') #要傳的paylo
 msg.attach(p)
 
 text = msg.as_string() 
-server.sendmail('m103040072@g-mail.nsysu.edu.tw','dyk31584@cdfaq.com',text)
+server.sendmail('a@g-mail.nsysu.edu.tw','wyi67441@nezid.com',text) #真正寄，回送寄件備份
